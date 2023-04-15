4681bc3093a141aea48aa1cd1790a9ccb1fdafe1
Chetan Tiwari <38131467+chetan915719@users.noreply.github.com>
2023-04-15 17:15:16 +0530

    aria label added

--- src/app/(components)/(sections)/intro.tsx
+++ src/app/(components)/(sections)/intro.tsx
@@ -7,9 +7,7 @@ export default function Introduction() {
     <section id="intro" className="h-screen">
       <div className="flex flex-wrap justify-between mx-auto h-screen items-center">
         <div className="w-full order-2 lg:w-1/2 lg:order-1">
-          <p className="mb-1 font-mono text-4xl md:text-6xl">
-            Hi, I&apos;m
-          </p>{" "}
+          <p className="mb-1 font-mono text-4xl md:text-6xl">Hi, I&apos;m</p>{" "}
           <br className="block md:hidden" />
           <p
             id="name"
@@ -24,18 +22,24 @@ export default function Introduction() {
             Full Stack Developer | Data Engineer | Traveller
           </p>
           <div className="flex my-10">
-            <a href="https://github.com/chetan915719" className="mx-3">
+            <a
+              href="https://github.com/chetan915719"
+              className="mx-3"
+              aria-label="Github"
+            >
               <FaGithub size={32} />
             </a>
             <a
               href="https://www.linkedin.com/in/chetan-tiwari-5055233/"
               className="mx-3"
+              aria-label="Linkedin"
             >
               <FaLinkedin size={32} />
             </a>
             <a
               href="https://www.xing.com/profile/Chetan_Tiwari2"
               className="mx-3"
+              aria-label="Xing"
             >
               <FaXing size={32} />
             </a>
